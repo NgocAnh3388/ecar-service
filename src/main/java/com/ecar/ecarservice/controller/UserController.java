0ec6056a13a019ea6bb66eca065a4eda2543de13
Ngoc Han <lengochan090105@gmail.com>
2025-10-28 08:35:16 +0700

    Role Technician
    role technician
    car model
    car segment
    confirm booking
    update status booking
    update database

--- src/main/java/com/ecar/ecarservice/controller/UserController.java
+++ src/main/java/com/ecar/ecarservice/controller/UserController.java
@@ -1,9 +1,14 @@
 package com.ecar.ecarservice.controller;
 
 import com.ecar.ecarservice.dto.UserDto;
+import com.ecar.ecarservice.enitiies.AppUser;
 import com.ecar.ecarservice.enums.AppRole;
+import com.ecar.ecarservice.payload.requests.UserSearchRequest;
 import com.ecar.ecarservice.service.UserService;
+import org.springframework.data.domain.Page;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -11,6 +16,7 @@ import java.util.Set;
 
 @RestController
 @RequestMapping("/api/admin/users")
+@PreAuthorize("hasRole('ADMIN')")
 public class UserController {
     private final UserService userService;
 
@@ -18,29 +24,32 @@ public class UserController {
         this.userService = userService;
     }
 
-    // 1. Get List of Users
     @GetMapping
     public ResponseEntity<List<UserDto>> getAllUsers() {
         return ResponseEntity.ok(userService.getAllUsers());
     }
 
-    // 2. Get User with ID
     @GetMapping("/{id}")
     public ResponseEntity<UserDto> getUserById(@PathVariable Long id) {
         return ResponseEntity.ok(userService.getUserById(id));
     }
 
-    // 3. Update User (ví dụ: chỉ cập nhật role)
     @PutMapping("/{id}")
     public ResponseEntity<UserDto> updateUserRoles(@PathVariable Long id, @RequestBody Set<AppRole> roles) {
         return ResponseEntity.ok(userService.updateUserRoles(id, roles));
     }
 
-    // 4. Delete User
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
         userService.deleteUser(id);
-        return ResponseEntity.noContent().build(); // Trả về 204 No Content
+        return ResponseEntity.noContent().build();
+    }
+
+    @RequestMapping(value = "/search", method = RequestMethod.POST)
+    @PreAuthorize("hasRole('ADMIN')")
+    public ResponseEntity<Page<AppUser>> searchUsers(@RequestBody UserSearchRequest request) {
+        Page<AppUser> searchResult = userService.searchUsers(request);
+        return new  ResponseEntity<>(searchResult, HttpStatus.OK);
     }
 
 }
