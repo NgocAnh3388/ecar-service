6666fc4acee9f8eee3a1a00bb9ee2866beedfc79
Ngoc Han <lengochan090105@gmail.com>
2025-10-28 08:46:52 +0700

    update

--- src/main/java/com/ecar/ecarservice/repositories/ServiceRecordRepository.java
+++ src/main/java/com/ecar/ecarservice/repositories/ServiceRecordRepository.java
@@ -2,9 +2,11 @@ package com.ecar.ecarservice.repositories;
 
 import com.ecar.ecarservice.enitiies.ServiceRecord;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
+@Repository
 public interface ServiceRecordRepository extends JpaRepository<ServiceRecord, Long> {
     List<ServiceRecord> findByLicensePlateOrderByServiceDateDesc(String licensePlate);
 }
