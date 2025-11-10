38e6159aedf47b2f32d5ca0344454f35a6504c13
Ngoc Han <lengochan090105@gmail.com>
2025-11-10 21:11:46 +0700

    sua vai cho

--- src/main/java/com/ecar/ecarservice/payload/requests/VehicleRequest.java
+++ src/main/java/com/ecar/ecarservice/payload/requests/VehicleRequest.java
@@ -12,6 +12,8 @@ public record VehicleRequest(
         @Size(min = 4, max = 15, message = "License plate must be between 4 and 15 characters")
         String licensePlate,
 
-        @Size(max = 17, message = "VIN number must not exceed 17 characters")
-        String vinNumber) {
+        @NotBlank(message = "VIN number cannot be blank")
+        @Size(min = 17, max = 17, message = "VIN number must be exactly 17 characters")
+        String vinNumber
+) {
 }
